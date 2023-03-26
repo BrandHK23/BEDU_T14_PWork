d870a291916d1af6c0e304b014dc69155cbb8c6f
Hector Peñaloza <hector.penaloza07@gmail.com>
2023-03-26 17:03:05 -0600

    Agregamos command Line Runner

--- Proyecto/src/main/java/org/bedu/java/backend/Postwork/service/AgendaService.java
+++ Proyecto/src/main/java/org/bedu/java/backend/Postwork/service/AgendaService.java
@@ -8,19 +8,20 @@ import org.bedu.java.backend.Postwork.model.Persona;
 
 import java.util.List;
 import java.util.Set;//validar
+import java.util.stream.Collectors;
 
 @Service
 public class AgendaService {
     private final ValidaTel validaTel;
+    private final FormatoTel formatoTel;
     private final PersonaRepository personaRepository;
 
 
     @Autowired
-    public AgendaService(ValidaTel validaTel, PersonaRepository personaRepository){
+    public AgendaService(ValidaTel validaTel, PersonaRepository personaRepository, FormatoTel formatoTel){
         this.validaTel=validaTel;
         this.personaRepository=personaRepository;
-
-
+        this.formatoTel = formatoTel;
     }
 
     public Persona guardaPersona(Persona persona){
@@ -29,8 +30,13 @@ public class AgendaService {
        return personaRepository.save(persona);
     }//fin persona
 
-    public List<Persona>getPersonas(){
-        return personaRepository.findAll(Sort.by("nombre"));
+    public List<Persona> getPersonas(){
+        return personaRepository.findAll(Sort.by("nombre"))
+                .stream()
+                .map(registro -> {
+                    registro.setTelefono(formatoTel.formatea(registro.getTelefono()));
+                    return registro;
+                }).collect(Collectors.toList());
     }
 
     public void borraPersona(Long Id) {
